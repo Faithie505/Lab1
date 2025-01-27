95b1c73aa3ae3db6302fbc51e0dec5069cb36b04
Faithie505 <faitholami16@gmail.com>
2025-01-27 13:46:39 +0000

    Added comments to lab 4

--- ex4.js
+++ ex4.js
@@ -1,23 +1,27 @@
 // function that takes in a string and returns a number
 function countString(value) {
-    return value.length;
+    return value.length; //returns a number (the fulllength of the word)
 }
+//function that removes any leading/ending white space from a string
 function countTrim(value) {
-    return value.trim().length;
+    return value.trim().length; //returns length of strig without first or last spaces
 }
+//function takes in bool (a true or false) and string (a word)
 function combineCount(space, word) {
-    //is the user wants to include leading and trailing spaces, calls method countString
+    //if the boolean is true, the user wants to include leading and trailing spaces
     if (space == true) {
         return word.length;
     }
-    //if the user wants to exclude them, calls the method to remove the spaces
+    //if the boolean is false, the user wants to exclude them, calls the method to remove the spaces
     else if (space == false) {
-        return word.trim().length;
+        return word.trim().length; //trims the length of the string
     }
-    return word.length;
+    else {
+        return word.length;
+    }
 }
-//display the 
-///console.log(countString(" test "));
-//console.log(countTrim(" test "));
+//display the length of the strings
+console.log(countString(" test "));
+console.log(countTrim(" test "));
 console.log(combineCount(true, " test "));
 console.log(combineCount(false, " test "));
